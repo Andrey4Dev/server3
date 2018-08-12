4b27f48b160125168f50c79a8f0ec28318f51a65
Andrey Pak <andrey.g.park@gmail.com>
2018-08-13 03:20:08 +1000

    Fix ststic files

--- server3/settings.py
+++ server3/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'2
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
